f78a87b06c8f9544baaf352f35bf339feaeaec98
Szymon Pyżalski <szymon@pythonista.net>
2017-11-26 13:42:42 +0100

    Lekcja 1 - typy

---
+++ slajdy/zadanie1.bash
@@ -0,0 +1,3 @@
+$ git clone git@github.com:zefciu/vavatech_szkolenie.git
+$ cd vavatech_szkolenie/zadania
+$ kate zadanie1.py
---
+++ zadania/zadanie1.py
@@ -0,0 +1,7 @@
+HERMENEGILDA = ('Hermenegilda', 'Końciubińska')
+ANTONI = ('Antoni', 'Gżegżółka')
+
+# 1. Wypisz imię i nazwisko obu osób ze spacją
+# 2. Sprawdź która z osób ma dłuższe nazwisko
+# 3. Wypisz imiona i nazwiska w odwrotnej kolejności
+# 4. Wypisz imiona obu osób WERSALIKAMI
