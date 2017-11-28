62075f752d917ba6e33abe8eccd07fcaa8a77fcf
Szymon Pyżalski <szymon@pythonista.net>
2017-11-28 10:38:55 +0100

    Lekcja 1 - zrobiona

---
+++ zadania/zadanie2.py
@@ -0,0 +1,31 @@
+GOŚCIE = [
+    'Marek',
+    'Joanna',
+    'Krzysztof',
+    'Agnieszka',
+    'Grzegorz',
+    'Justyna'
+]
+
+# 1. Wypisz liczbę gości
+# 2. Wypisz pierwszych trzech gości
+# 3. Dodaj imię "Katarzyna" do listy
+# 4. Utwórz kopię listy gości
+# 5. Dodaj imię "Natalia" do jednej z list
+
+PAŃSTWA = {
+    'Wielka Brytania': 'Great Britain',
+    'Niemcy': 'Deutchland',
+    'Rosja': 'Россия',
+    'Gruzja': 'საქართველო',
+}
+
+PAŃSTWA_2 = {
+    'Grecja': 'Ελλάδα',
+    'Francja': 'France',
+    'Finlandia': 'Suomi',
+}
+
+# 6. Wypisz nazwę Rosji po rosyjsku
+# 7. Dodaj nazwę dla Włoch "Italia"
+# 8. Uzupełnij słownik PAŃSTWA o słownik PAŃSTWA_2
