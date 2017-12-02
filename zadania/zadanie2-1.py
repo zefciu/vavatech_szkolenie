a9bfad024ec90161fa99544a04582827900e48e6
Szymon Pyżalski <szymon@pythonista.net>
2017-12-02 15:07:21 +0100

    Builtins

---
+++ zadania/zadanie2-1.py
@@ -0,0 +1,14 @@
+PAŃSTWA = {
+    'Wielka Brytania': 'Great Britain',
+    'Niemcy': 'Deutchland',
+    'Rosja': 'Россия',
+    'Gruzja': 'საქართველო',
+    'Grecja': 'Ελλάδα',
+    'Francja': 'France',
+    'Finlandia': 'Suomi',
+}
+
+# 1. Wypisz wszystkie nazwy państw w obcych językach
+# 2. Wypisz nazwy parami "nazwa polska - nazwa obca"
+# 3. Wypisz polskie nazwy o długości krótszej bądź równej 6 liter
+# 4. Wypisz wszystkie liczby ciągu Fibonacciego mniejsze lub równie 1000
