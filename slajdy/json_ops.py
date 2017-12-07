0b4692d0b80ccb646f96e33d7286f70796b94422
Szymon Pyżalski <szymon@pythonista.net>
2017-12-08 00:35:37 +0100

    Skrypt

--- slajdy/json_ops.py
+++ slajdy/json_ops.py
@@ -1,8 +1,8 @@
 import json
 
 with open('MOCK_DATA.json') as f:
-    print(json.read(f))
+    print(json.load(f))
 
 with open('new_data.json', 'w') as f:
-    json.dump({'name': 'Jon', 'surname': 'Snow'})
+    json.dump({'name': 'Jon', 'surname': 'Snow'}, f)
 
